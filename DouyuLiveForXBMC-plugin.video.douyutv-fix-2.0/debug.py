6c2fddd39489b92dfc7a68b563fcf3c7b0379439
0987363 <0987363@gmail.com>
2015-10-20 19:42:10 +0800

    rename project

--- DouyuLiveForXBMC-plugin.video.douyutv-fix-2.0/debug.py
+++
@@ -1,7 +0,0 @@
-import DouyuAPI
-
-__author__ = 'hexpang'
-helper = DouyuAPI.DouyuAPI()
-result = helper.loadCategory() #helper.request("login",{"username":"test","password":"test","type":"md5"})
-print result
-print "Done"
