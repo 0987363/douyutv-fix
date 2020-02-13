d35b749d10a6f75c3edd20dd7ffd27ecf19b1e26
0987363@gmail.com <0987363@gmail.com>
2020-02-13 11:05:26 +0800

    remove urllib2

--- APIHelper.py
+++ APIHelper.py
@@ -1,30 +1,26 @@
 # -*- coding: utf-8 -*-
-# 斗鱼接口
-import urllib2
 import urllib
-import json
-import md5
-import time
+import requests
+#import json
+#import time
 
 
 class APIHelper:
     def __init__(self):
         self.baseUrl = "http://capi.douyucdn.cn/api/v1/"
 
-    def GetStringMD5(self, str):  
-        m = md5.new()  
-        m.update(str)  
-        return m.hexdigest()  
-
     def request(self, action, param=None):
         reqUrl = self.baseUrl + action
         if param != None:
             reqUrl = reqUrl + "?" + urllib.urlencode(param)
 
-        header = {"User-Agent" : "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"}
-        request = urllib2.Request(reqUrl, headers = header)
-        response = urllib2.urlopen(request, timeout=10).read()
-        jsonObject = json.loads(response)
+        header = {
+            "User-Agent" : "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;",
+        }
+        response = requests.get(url=reqUrl, headers=header)
+        jsonObject = response.json()
+
+#        jsonObject = json.loads(response)
         data = jsonObject["data"]
         return data
 
